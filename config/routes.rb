7048b84d49a9d7e3a27a22d2f4a269fb6ba954fc
Eric May <eric.may@boldpenguin.com>
2023-02-06 11:20:50 -0500

    added mockups

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   get "/pages/:page" => "pages#show"
   get 'builders' => 'pages#builders'
+  get 'neighborhoods' => 'pages#neighborhoods'
+  get 'resources' => 'pages#resources'
 end
